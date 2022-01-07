97840cc157b89ce59859f0036df24e19a2ff1206
wly <1355498705@qq.com>
2022-01-07 15:17:59 +0800

    引入pinia 并使用

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <div class="test">demo</div>
+  <div>{{ mainStore.name }}</div>
 </template>
+
+<script setup lang="ts">
+import { useMainStore } from "@/store/main";
+const mainStore = useMainStore();
+</script>
 
 <style lang="scss" scoped>
 .test {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+// 引入
+import { createPinia } from "pinia";
+//创建根存储库并将其传递给应用程序
+app.use(createPinia());
 
 app.use(router);
 
